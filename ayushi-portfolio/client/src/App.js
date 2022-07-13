787f03be11ab9c844bf5fad77a32038a4acbf555
ayushiM2020 <89742259+ayushiM2020@users.noreply.github.com>
2022-07-13 04:29:31 -0600

    navbar added

--- ayushi-portfolio/client/src/App.js
+++ ayushi-portfolio/client/src/App.js
@@ -1,9 +1,23 @@
-import Profile from "./container/Home/Profile";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home1 from "./pages/Home1";
+import Projects from "./pages/Projects";
+import Experience from "./pages/Experience";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+// import ProjectDisplay from "./pages/ProjectDisplay";
 
 function App() {
   return (
     <div className="App">
-      <Profile/>
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home1 />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/experience" element={<Experience />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ ayushi-portfolio/client/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="toggleButton">
+        <button></button>
+      </div>
+      <div className="links">
+        <a href="/"> Home</a>
+        <a href="/projects"> Projects</a>
+        <a href="/experience"> Experience</a>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
